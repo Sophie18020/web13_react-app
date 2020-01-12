558c9d2f3977b523361da983dbf684114de46660
Sophie18020 <boltenkova00@icloud.com>
2020-01-12 18:55:53 +0300

    Updates

---
+++ precache-manifest.bf3c5880cd4db51419ee21002395a332.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d0ed60faa608397c1533e8d439be73",
+    "url": "/web13_react-app/index.html"
+  },
+  {
+    "revision": "e5cf727c67c8bd8a1cc0",
+    "url": "/web13_react-app/static/js/2.2e4c0064.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/web13_react-app/static/js/2.2e4c0064.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3ca5d18c7aa811dd602",
+    "url": "/web13_react-app/static/js/main.505a6068.chunk.js"
+  },
+  {
+    "revision": "c03a98fa3676f52f9f0b",
+    "url": "/web13_react-app/static/js/runtime-main.31d3541b.js"
+  }
+]);
